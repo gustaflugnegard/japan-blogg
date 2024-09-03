360d8468d32ca0776fc29c58be46c53ed297c231
gustaflugnegard <gustaf.lugnegard@gmail.com>
2024-09-03 16:00:57 +0200

    minor change with maps

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -19,12 +19,9 @@ const StyledIframe = styled.iframe`
 const GoogleMaps = () => {
   return (
     <MapContainer>
-      <StyledIframe
-        title="Custom Map"
-        src="https://www.google.com/maps/d/embed?mid=1lDv8KCH6L-epkKZtgKyHOsPeauzAeHQ&hl=sv&ehbc=2E312F"
-        allowFullScreen
-        loading="lazy"
-      />
+      <StyledIframe 
+        src="https://www.google.com/maps/d/u/7/embed?mid=1i452TqbaNBuyJq2mXgS-UWh9uJ2vsSo&ehbc=2E312F&noprof=1" >
+      </StyledIframe>
     </MapContainer>
   );
 };
